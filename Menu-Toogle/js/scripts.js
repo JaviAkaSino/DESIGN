1cef5d4d2f47878e0cc6181132059f0501746191
JaviAkaSino <javiakasino@hotmail.com>
2023-02-06 19:13:51 +0100

    ejer 3

---
+++ Menu-Toogle/js/scripts.js
@@ -0,0 +1,29 @@
+
+
+$(document).ready(function () {
+
+    //EJERCICIO 3
+
+    /* Al pasar el ratón por encima de cada item de menú, este
+    se resalte de alguna forma: cambie el color de fondo,
+    cambie el estilo de fuente a negrita,…
+
+    Cuando el ratón deje de estar sobre ese ítem debe volver
+    a su estado original.
+
+    Utiliza el método hover() para realizar esta tarea*/
+
+    $("a.item-menu").hover(
+        function () {
+
+        $(this).css("background-color", "white");
+        $(this).css("color", "black");
+
+    }, function(){
+
+        $(this).css("background-color", "black");
+        $(this).css("color", "white");
+    })
+});
+
+
