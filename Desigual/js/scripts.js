6de8b9be11e181c71b6374bf1fc7288ff785d5c1
JaviAkaSino <javiakasino@hotmail.com>
2023-02-08 14:38:28 +0100

    miau

--- Desigual/js/scripts.js
+++ Desigual/js/scripts.js
@@ -1,7 +1,21 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    $("div#hamburguesa").click(function(){
-
+    $("div#hamburguesa").click(function () {
         $("ul#menu").toggle();
     })
+
+    $(document).scroll(function () {
+
+        $("header").css({
+            "position": "fixed",
+            "width": "98%",
+            "background-color": "rgba(255, 255, 255, 0.75)",
+        })
+
+    })
+
+    $(window).resize(function(){
+        $("ul#menu").css("display", "none");
+    })
+
 })
