19a7f543b29c86341b67a2df487cf6d79c74e580
JaviAkaSino <javiakasino@hotmail.com>
2023-02-24 11:09:36 +0100

    df

--- Webusefab/js/script-justfub.js
+++ Webusefab/js/script-justfub.js
@@ -1,9 +1,18 @@
+$(document).ready(function () {
+
+    
     //EJERCICIO 7
     /*Modifica el HTML y añade el código CSS necesario para que el menú 
     aparezca desde la izquierda y desplace al resto de la página hacia la 
     derecha. El resto de la página se debe oscurecer para simular que se 
     encuentra en segundo plano. Se debe poder ocultar el menú haciendo clic 
     en cualquier posición de la página.*/
+
+
+    $("nav#menu-principal>span").onclick(function(){
+
+        
+    })
 
     //EJERCICIO 8
     /*Modifica el HTML y añade el código CSS necesario para que al 
@@ -13,3 +22,6 @@
     //EJERCICIO 9
     /*Investiga como usar un Plugin para crear un slider y utilízalo en 
     esta web. */
+
+
+})
